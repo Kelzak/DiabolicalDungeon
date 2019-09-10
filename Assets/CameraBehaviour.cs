93a0716b66481c224508e9292d1f69e0ae1b9734
Lachlan <LachlanRobertDuncan@gmail.com>
2019-09-10 16:38:20 -0500

    Camera Follows Player

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject player;
+    [RangeAttribute(0.0f, 1.0f)]
+    public float followStrictness;
+
+    private Vector3 target;
+
+    void Update()
+    {
+        target = player.transform.position + offset;
+
+        //determines what proportion of the distance to the player to move the camera, depending on deltaTime
+        float moveProportion = Mathf.Pow(followStrictness, Time.deltaTime);
+
+        //move the camera towards the player
+        transform.position = moveProportion * transform.position + (1.0f - moveProportion) * target;
+    }
+}
