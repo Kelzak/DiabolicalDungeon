2386cd706adf5b72f0641eebaad2935858c37201
Zackary Seiple <KelzakGames@gmail.com>
2019-09-12 15:13:52 -0500

    Enemy Script Created

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private Behaviour halo;
+
+    private void Start()
+    {
+        halo = (Behaviour)GetComponent("Halo");
+    }
+}
