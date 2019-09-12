87b3754bcc5cda4979f8de8273c3e065c8227981
Kyle Tucker <ktuckerjr@gmail.com>
2019-09-12 17:38:32 -0500

    Update WallShooter.cs

--- Assets/Scripts/WallShooter.cs
+++ Assets/Scripts/WallShooter.cs
@@ -40,12 +40,26 @@ public class WallShooter : MonoBehaviour
         }
     }
 
-
-
       void fire()
       {
           GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
           newBullet.GetComponent<Rigidbody>().AddRelativeForce(bulletDirection);
           Destroy(newBullet, 4.0f);
+      }
+      void OnTriggerEnter(Collider other)
+      {
+        if (other.tag == ("Player"))
+        {
+        playerIsInSideWallShotRange = true;
+          Debug.Log("Player is in shooting range");
+        }
+      }
+      void OnTriggerExit(Collider other)
+      {
+        if (other.tag == ("Player"))
+        {
+        playerIsInSideWallShotRange = false;
+          Debug.Log("Player has exited shooting range");
+        }
       }
 }
