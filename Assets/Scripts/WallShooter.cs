faa0f8439779d10ec77f10892f1a181bceeefc5e
Kyle Tucker <ktuckerjr@gmail.com>
2019-09-12 16:42:21 -0500

    Wall stuff

--- Assets/Scripts/WallShooter.cs
+++ Assets/Scripts/WallShooter.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class WallShooter : MonoBehaviour
 {
+  public GameObject bullet;
+  public float speedX = 0;
+  public float speedY = 0;
+  public float speedZ = 0;
+  float shotDelay = 1f;
+  float timeCheck;
+  public Vector3 bulletDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletDirection = (speedX, speedY, speedZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void FixedUpdate()
+    {
+      
+    }
+
+    if (/* player is in the WallShooter area && */ Time.time >= timeCheck)
+        {
+        fire();
+        timeCheck = Time.time + shotDelay;
+      }
+
+      void fire()
+      {
+          GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+          newBullet.GetComponent<Rigidbody>().AddRelativeForce(bulletDirection);
+          Destroy(newBullet, 4.0f);
+      }
 }
