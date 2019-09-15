4ffd34ec299b057997248e35ebc8c49543fa96a1
Zackary Seiple <KelzakGames@gmail.com>
2019-09-14 23:50:37 -0500

    Basic Navigation

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     private Behaviour halo;
+    private NavMeshAgent agent;
+    private LayerMask layerToIgnore;
+
+    private Transform player;
+
     private enum State { Idle, Attacking };
     private State currentState;
 
     private void Start()
     {
         halo = (Behaviour)GetComponent("Halo");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+
+        layerToIgnore = ~LayerMask.GetMask("Ignore Raycast");
         StartCoroutine(Idle());
+    }
+
+    private void LateUpdate()
+    {
+        if (currentState == State.Attacking)
+        {
+            var lookPos = new Vector3(player.position.x, this.transform.position.y, player.position.z);
+            transform.LookAt(lookPos);
+        }
     }
 
     public void MakeTarget(bool on)
@@ -23,13 +44,15 @@ public class EnemyBehaviour : MonoBehaviour
     {
 
         Vector3 playerPos;
+        agent.SetDestination(transform.position);
 
         while (currentState == State.Idle)
         {
-            playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+            playerPos = player.position;
+            
             RaycastHit hit;
-            Physics.Raycast(transform.position, (playerPos - transform.position).normalized, out hit, Mathf.Infinity);
-            if (hit.collider.tag == "Player")
+            Physics.Raycast(transform.position, (playerPos - transform.position).normalized, out hit, Mathf.Infinity, layerToIgnore);
+            if (hit.collider != null && hit.collider.tag == "Player")
             {
                 currentState = State.Attacking;
             }
@@ -47,9 +70,10 @@ public class EnemyBehaviour : MonoBehaviour
 
         while(currentState == State.Attacking)
         {
-            playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-            Physics.Raycast(transform.position, (playerPos - transform.position).normalized, out hit, Mathf.Infinity);
-            if (hit.collider.tag != "Player")
+            playerPos = player.position;
+            agent.SetDestination(playerPos);
+            Physics.Raycast(transform.position, (playerPos - transform.position).normalized, out hit, Mathf.Infinity, layerToIgnore);
+            if (hit.collider != null && hit.collider.tag != "Player")
             {
                 currentState = State.Idle;
             }
