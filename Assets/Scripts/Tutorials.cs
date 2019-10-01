d46d2b959efc7219a54054cc642d5861f82e524c
Kyle Tucker <ktuckerjr@gmail.com>
2019-10-01 13:03:34 -0500

    Update Tutorials.cs

--- Assets/Scripts/Tutorials.cs
+++ Assets/Scripts/Tutorials.cs
@@ -6,24 +6,51 @@ using UnityEngine.UI;
 public class Tutorials : MonoBehaviour
 {
     public Text Tutorial1;
+    private bool isInTutorialRange = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+
+    }
+
+    public void setTutorial1Text()
+    {
+      if (isInTutorialRange == false)
+      {
+        Tutorial1.gameObject.SetActive(false);
+      }
+
+      if(isInTutorialRange == true)
+      {
+        Tutorial1.gameObject.SetActive(true);
+      }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == ("Player"))
         {
-            Tutorial1.gameObject.SetActive(true);
-            Destroy(Tutorial1, 3f);
+            //Tutorial1.gameObject.SetActive(true);
+            //Destroy(Tutorial1, 5f);
+            isInTutorialRange = true;
+            Invoke("setTutorial1Text", 1);
         }
     }
-    
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            //Tutorial1.gameObject.SetActive(false);
+            isInTutorialRange = false
+            Invoke("setTutorial1Text", 1);
+            //Destroy(Tutorial1, 5f);
+        }
+    }
+
 }
