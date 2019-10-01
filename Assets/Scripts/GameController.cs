dd693dea7fe480211ebfd73288f6721762cb6a99
BrennanTCarlyle <brennan.carlyle@gmail.com>
2019-09-30 17:14:39 -0900

    Updated GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
    
-    public Texture2D cursorTexture;
+    public Texture2D cursorTextureRed;
+    public Texture2D cursorTextureGreen;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
     void OnMouseEnter()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.SetCursor(cursorTextureRed, hotSpot, cursorMode);
+    }
+
+
+    void OnMouseOver()
+    {
+        if (gameObject.tag == "Enemy")
+        {
+            Cursor.SetCursor(cursorTextureGreen, hotSpot, cursorMode);
+        }
     }
 
     void OnMouseExit()
     {
         Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
+    
 }
 
